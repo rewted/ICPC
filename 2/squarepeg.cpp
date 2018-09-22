875ffc3e93f22a7b9ae8a14b495f8ca684695760
James Culver <rewtguy@gmail.com>
2018-09-22 11:51:08 -0700

    Squarepeg attempt

---
+++ 2/squarepeg.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+void get_data(int count, vector<int> * data) {
+  int temp = 0;
+  for (int i = 0; i < count; i++) {
+    cin >> temp;
+    data->push_back(temp);
+  }
+  sort(data->begin(), data->end());
+}
+
+void print_data (vector<int> * data) {
+  auto itr = data->begin();
+  while (itr != data->end()) {
+    cout << *itr << endl;
+    itr++;
+  }
+}
+
+double find_hypot (int a) {
+  return a * sqrt(2);
+}
+
+int plots_filled_with_houses(vector<int> * plots, vector<int> * circles, vector<int> * squares) {
+  int plots_filled = 0;
+  auto plots_itr = plots->begin();
+  auto circles_itr = circles->begin();
+  auto squares_itr = squares->begin();
+  while (plots_itr != plots->end()) {
+    // does house fit in plot?
+    if (*circles_itr < *plots_itr) {
+      plots_filled++;
+      circles_itr++;
+    }
+    else {
+      if (find_hypot(*squares_itr) < *plots_itr) {
+	plots_filled++;
+	squares_itr++;
+      }
+    }
+    plots_itr++;
+  }
+
+  return plots_filled;
+}
+
+int main () {
+  int plots = 0;
+  int circle_homes = 0;
+  int square_homes = 0;
+
+  vector<int> plots_radius {};
+  vector<int> circle_homes_radius {};
+  vector<int> square_homes_side {};
+  
+  cin >> plots;
+  cin >> circle_homes;
+  cin >> square_homes;
+
+  get_data(plots, &plots_radius);
+  get_data(circle_homes, &circle_homes_radius);
+  get_data(square_homes, &square_homes_side);
+
+  cout << plots_filled_with_houses(&plots_radius, &circle_homes_radius, &square_homes_side) << endl;
+
+  return 0;
+}
