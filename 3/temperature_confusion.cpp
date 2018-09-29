c05e17856eaa4b97c582809f0730254c5627214c
James Culver <rewtguy@gmail.com>
2018-09-29 10:30:16 -0700

    Temperature confusion attempt 1

---
+++ 3/temperature_confusion.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+int gcd (int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  return gcd(b, a % b);  
+}
+
+using namespace std;
+
+int main () {
+  string a;
+  cin >> a;
+  //cout << a << endl;
+  bool first_half = true;
+  string beginning;
+  string end;
+  for (auto x: a) {
+    if (!first_half) {
+      end += x;
+    }
+    if (x == '/') {
+      first_half = false;
+    }
+    if (first_half) {
+      beginning += x;
+    }
+  }
+  int num = stoi(beginning);
+  int dem = stoi(end);
+
+  int b = 32;
+  b *= dem;
+  num -= b;
+  num *= 5;
+  dem *= 9;
+  
+  int greatest_divisor = gcd(num, dem);
+  cout << num/greatest_divisor << "/" << dem/greatest_divisor << endl;
+  return 0;
+}
